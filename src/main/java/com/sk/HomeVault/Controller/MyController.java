fcefef93ba9a32fdb361c5c05bd011830394edd3
Sumit kumar <sk1897314@gmail.com>
2026-07-06 17:41:02 +0530

    /feature/added get , getAll , getById

--- src/main/java/com/sk/HomeVault/Controller/MyController.java
+++ src/main/java/com/sk/HomeVault/Controller/MyController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/vault/")
 public class MyController {
@@ -24,6 +26,14 @@ public class MyController {
      return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
+    @GetMapping("/items")
+    public ResponseEntity<List<ResponseDto>> allItems(){
+        return ResponseEntity.ok(itemService.allItems());
+    }
 
-
-}
+        @GetMapping("/item/{id}")
+        public ResponseEntity<ResponseDto> item(@PathVariable long id){
+          ResponseDto responseDto = itemService.itemById(id);
+          return ResponseEntity.ok(responseDto);
+        }
+    }
